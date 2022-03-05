091b9df5ae3ec5be192c08498ba1e9ea2cbf51da
r-fine <saiadul.arfain@tuta.io>
2022-03-06 04:31:19 +0600

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        if (Auth::user()->hasRole('admin')) {
+            return view('admin.admin_homepage');
+        }elseif(Auth::user()->hasRole('s_provider') && Auth::user()->is_s_provider==1) {
+            return view('dashboard');
+        }elseif(Auth::user()->hasRole('customer')) {
+            return view('dashboard');
+        }
+    }
+}
