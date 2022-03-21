fc4417bf65565e128f39041e0bca65e483ee9c7c
r-fine <saiadul.arfain@tuta.io>
2022-03-22 04:55:36 +0600

    added feature: verify service providers manually via admin

---
+++ resources/views/admin/verify_provider.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts/admin_dashboard')
+@section('admin_content')
+@if (Session::has('success'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>{{ Session::get('success') }}</strong>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+<div class="container my-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <h2 class="text-danger text-center fw-bolder mb-5">Unverified Providers' List</h2>
+        <div class="container my-3">
+            <table class="table table-bordered">
+                <tr>
+                    <th>Id</th>
+                    <th>Full Name</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Verify</th>
+                </tr>
+                @foreach ($users as $u)
+                <tr>
+                    <td>{{ $u->id }}</td>
+                    <td>{{ $u->address->full_name }}</td>
+                    <td>{{ $u->address->phone }}</td>
+                    <td>{{ $u->address->getFullAddress() }}</td>
+                    <td>
+                        {{-- <form action="{{ route('verify.user', $u->id) }}" method="POST">
+        
+                        </form> --}}
+                        <a href="{{ route('admin.verify.user', $u->id) }}" class="text-success fs-3">
+                            <i class="bi bi-check-square-fill"></i>
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+    
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 /*
@@ -23,6 +24,8 @@ Route::prefix('admin')->group(function () {
     Route::name('admin.')->group(function () {
         Route::group(['middleware' => ['auth', 'role:admin']], function () {
             Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+            Route::get('/user-verification/{id}', [AdminController::class, 'user_verification'])->name('verify.user');
+            Route::get('/unverified-user-list', [AdminController::class, 'show_unverified_providers'])->name('unverified.provider.list');
         });
     });
 });
