6f31438942261bf799bffd0e8a157c0af6161b2c
r-fine <saiadul.arfain@tuta.io>
2022-03-27 23:59:43 +0600

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $categories = Category::all();
         view()->share('categories', $categories);
+
+        Paginator::useBootstrapFive();
     }
 }
