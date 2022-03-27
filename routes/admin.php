aa9506d8d34d57b9f777756f9feff928461ee7c8
r-fine <saiadul.arfain@tuta.io>
2022-03-27 15:37:54 +0600

    admin routes for service CRUD operations

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 // Routes for Admin
@@ -22,6 +23,17 @@ Route::prefix('admin')->group(function () {
                     Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('edit');
                     Route::put('/{category}', [CategoryController::class, 'update'])->name('update');
                     Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy');
+                });
+            });
+            // Routes for Service CRUD
+            Route::prefix('services')->group(function () {
+                Route::name('services.')->group(function () {
+                    Route::get('/', [ServiceController::class, 'index'])->name('index');
+                    Route::get('/create', [ServiceController::class, 'create'])->name('create');
+                    Route::post('/store', [ServiceController::class, 'store'])->name('store');
+                    Route::get('/{service}/edit', [ServiceController::class, 'edit'])->name('edit');
+                    Route::put('/{service}', [ServiceController::class, 'update'])->name('update');
+                    Route::delete('/{service}', [ServiceController::class, 'destroy'])->name('destroy');
                 });
             });
         });
